0e5437bd6e5dfb8ff8666180771d5df75f62a8af
Chuck Ocheret <ocheret@panix.com>
2024-08-27 13:08:53 -0400

    Nerd. Rel num. Scroll off. Lang servers. %. Tree.
    diff --git a/init.lua b/init.lua
    index 13ea93a..6c06eba 100644
    --- a/init.lua
    +++ b/init.lua
    @@ -91,7 +91,7 @@ vim.g.mapleader = ' '
     vim.g.maplocalleader = ' '
    
     -- Set to true if you have a Nerd Font installed and selected in the terminal
    -vim.g.have_nerd_font = false
    +vim.g.have_nerd_font = true
    
     -- [[ Setting options ]]
     -- See `:help vim.opt`
    @@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
     vim.opt.number = true
     -- You can also add relative line numbers, to help with jumping.
     --  Experiment for yourself to see if you like it!
    --- vim.opt.relativenumber = true
    +vim.opt.relativenumber = true
    
     -- Enable mouse mode, can be useful for resizing splits for example!
     vim.opt.mouse = 'a'
    @@ -155,7 +155,7 @@ vim.opt.inccommand = 'split'
     vim.opt.cursorline = true
    
     -- Minimal number of screen lines to keep above and below the cursor.
    -vim.opt.scrolloff = 10
    +vim.opt.scrolloff = 1
    
     -- [[ Basic Keymaps ]]
     --  See `:help vim.keymap.set()`
    @@ -607,10 +607,10 @@ require('lazy').setup({
           --  - settings (table): Override the default settings passed when initializing the server.
           --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
           local servers = {
    -        -- clangd = {},
    -        -- gopls = {},
    -        -- pyright = {},
    -        -- rust_analyzer = {},
    +        clangd = {},
    +        gopls = {},
    +        pyright = {},
    +        rust_analyzer = {},
             -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
             --
             -- Some languages (like typescript) have entire language plugins that can be useful:
    @@ -871,7 +871,7 @@ require('lazy').setup({
           -- cursor location to LINE:COLUMN
           ---@diagnostic disable-next-line: duplicate-set-field
           statusline.section_location = function()
    -        return '%2l:%-2v'
    +        return '%3p%% %2l:%-2v'
           end
    
           -- ... and there is more!
    @@ -917,7 +917,7 @@ require('lazy').setup({
       -- require 'kickstart.plugins.indent_line',
       -- require 'kickstart.plugins.lint',
       -- require 'kickstart.plugins.autopairs',
    -  -- require 'kickstart.plugins.neo-tree',
    +  require 'kickstart.plugins.neo-tree',
       -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
    
       -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -155,7 +155,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 1
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -607,10 +607,10 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        clangd = {},
+        gopls = {},
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -871,7 +871,7 @@ require('lazy').setup({
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return '%3p%% %2l:%-2v'
       end
 
       -- ... and there is more!
@@ -917,7 +917,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
